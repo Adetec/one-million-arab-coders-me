ddc73f70a797099ca816312c3cf0faec0de59ed0
Adel Lassag <adetech.aadl@gmail.com>
2018-03-26 02:23:11 +0100

    add cookies functions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,48 @@
-var visitor = prompt('Please, enter your name')
-swal('Hello '+visitor);
-document.getElementById('visitor').innerHTML='Welcome '+visitor+'!';
+
+
+//cooooooookieeeees:
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*12*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var visitor=getCookie("Visitor");
+  if (visitor != "") {
+    swal('Welcome again '+visitor);
+  } else {
+     visitor = prompt("Please enter your name:","");
+     if (visitor != "" && visitor != null) {
+         setCookie("Visitor", visitor, 30);
+         swal('Hello '+visitor+'\nYour name is saved in your browser!');
+     }
+     
+  }
+  document.getElementById('visitor').innerHTML='Welcome '+visitor+'!'
+}
+
+
+
+
+
 
 
 
@@ -46,7 +88,7 @@ aboutMe.addEventListener('click', function(){
   }
 });
 
-
+// My works shops:
 var worksShops = [
  {
    id: 'clone-omac',
